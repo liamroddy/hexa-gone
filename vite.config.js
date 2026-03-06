b59b7f773387c2346c45c531e2fe6e2d46fbae97
Liam Roddy <sgt.p2pper@gmail.com>
2026-03-06 15:28:14 +0000

    fix url base typo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hexagone/',
+  base: '/hexa-gone/',
   plugins: [react()],
 })
